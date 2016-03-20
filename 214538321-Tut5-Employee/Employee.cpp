0cd54525e73451835cd37eee43f647bf4d4fbfb7
samkelo <sumcarelow@gmail.com>
2016-03-20 15:01:00 +0200

    created clas Employee

---
+++ 214538321-Tut5-Employee/Employee.cpp
@@ -0,0 +1,15 @@
+#include "Emplyee.h"
+
+void Employee::setName(string name){
+	newNameOfEmployee = name;
+}
+
+void Employee::setNumber(int staffNumber){
+	newStaffNumber = staffNumber;
+}
+
+string Employee::name(){
+	return newNameOfEmployee;
+
+}
+
